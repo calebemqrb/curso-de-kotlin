9f88f7887430a9ff0252678e5e1e1a81cb6a347f
Calebe-Marques <calebe.reboucas@sptech.school>
2024-01-18 22:29:54 -0300

    feat: tipos de dados

---
+++ primeiro-projeto/src/main/kotlin/TiposDeDados.kt
@@ -0,0 +1,19 @@
+fun main() {
+    println("Double -> MAX: ${Double.MAX_VALUE} - MIN ${Double.MIN_VALUE}")
+    println("Float -> MAX: ${Float.MAX_VALUE} - MIN ${Float.MIN_VALUE}")
+    println("Long -> MAX: ${Long.MAX_VALUE} - MIN ${Long.MIN_VALUE}")
+    println("Int -> MAX: ${Int.MAX_VALUE} - MIN ${Int.MIN_VALUE}")
+    println("Short -> MAX: ${Short.MAX_VALUE} - MIN ${Short.MIN_VALUE}")
+    println("Byte -> MAX: ${Byte.MAX_VALUE} - MIN ${Byte.MIN_VALUE}")
+
+    var char: Char = 'A'
+    var str: String = "Isso é uma string"
+    var boolean: Boolean = true
+
+    var double: Double = 10.0
+    var float: Float = 10f
+    var long: Long = 10L
+    var int: Int = 10
+    var short: Short = 10
+    var byte: Byte = 10
+}
