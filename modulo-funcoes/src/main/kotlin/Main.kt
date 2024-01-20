9e5ca0a44c852f823b039d522fc34e06f1635f52
Calebe-Marques <calebe.reboucas@sptech.school>
2024-01-20 16:50:09 -0300

    feat: introducao a funcoes

---
+++ modulo-funcoes/src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    helloWorld()
+    println(sum(
+        10,
+        10
+    ))
+}
+
+fun helloWorld() {
+    println("Hello, World!")
+}
+
+fun sum(a: Int, b: Int): Int{
+    return (a + b)
+}
