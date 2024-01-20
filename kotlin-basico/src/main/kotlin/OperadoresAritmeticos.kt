8a1e83fb5fb728832474055c31bef39cd9c59126
Calebe-Marques <calebe.reboucas@sptech.school>
2024-01-20 12:03:28 -0300

    feat: operadores aritmeticos

---
+++ kotlin-basico/src/main/kotlin/OperadoresAritmeticos.kt
@@ -0,0 +1,18 @@
+fun main(){
+    // +, -, *, /, %
+
+    var idade = 19
+    idade += 20
+    println(idade)
+    idade -= 5
+    println(idade)
+    idade *= 2
+    println(idade)
+    idade /= 4
+    println(idade)
+
+    idade = 10 % 3
+    println(idade)
+
+    println(++idade)
+}
