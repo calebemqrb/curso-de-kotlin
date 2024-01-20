0a0d0b85c50946d32b65608889538a41fab3588e
Calebe Marques <110927947+calebemqrb@users.noreply.github.com>
2024-01-20 19:48:18 -0300

    Delete primeiro-projeto directory

--- primeiro-projeto/src/main/kotlin/TipoUnsigned.kt
+++
@@ -1,6 +0,0 @@
-fun main(){
-    println("ULong -> MAX: ${ULong.MAX_VALUE} - MIN ${ULong.MIN_VALUE}")
-    println("UInt -> MAX: ${UInt.MAX_VALUE} - MIN ${UInt.MIN_VALUE}")
-    println("UShort -> MAX: ${UShort.MAX_VALUE} - MIN ${UShort.MIN_VALUE}")
-    println("UByte -> MAX: ${UByte.MAX_VALUE} - MIN ${UByte.MIN_VALUE}")
-}
--- primeiro-projeto/src/main/kotlin/Variaveis.kt
+++
@@ -1,8 +0,0 @@
-fun main(){
-    var nome = "Nome"
-    nome = "Nome alterado"
-    println(nome)
-
-    val nome2 = "Nome inalterável"
-    println(nome2)
-}
