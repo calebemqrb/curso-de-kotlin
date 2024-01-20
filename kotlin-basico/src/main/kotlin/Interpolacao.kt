b99156353e0b31c46ee85764c11d194d7e6fec9e
Calebe-Marques <calebe.reboucas@sptech.school>
2024-01-20 11:46:04 -0300

    feat: interpolacao

---
+++ kotlin-basico/src/main/kotlin/Interpolacao.kt
@@ -0,0 +1,8 @@
+fun main(){
+    val nome = "Nome"
+    val sobrenome = "Sobrenome"
+
+    println("Olá, " + nome + " " + sobrenome)
+    println("Olá, $nome $sobrenome")
+    println("Double: ${Double.MAX_VALUE}")
+}
---
+++ kotlin-basico/src/main/kotlin/Variaveis.kt
@@ -0,0 +1,8 @@
+fun main(){
+    var nome = "Nome"
+    nome = "Nome alterado"
+    println(nome)
+
+    val nome2 = "Nome inalterável"
+    println(nome2)
+}
