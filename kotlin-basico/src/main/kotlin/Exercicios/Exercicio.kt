c5fa65f4327cd6bd6e8eb73ef1f08bb9a63eef6c
Calebe-Marques <calebe.reboucas@sptech.school>
2024-01-20 12:16:13 -0300

    feat: exercicio do modulo basico

---
+++ kotlin-basico/src/main/kotlin/Exercicios/Exercicio.kt
@@ -0,0 +1,14 @@
+package Exercicios
+
+fun main(){
+    var nomeCompleto: String = "Nome Sobrenome"
+    println(nomeCompleto)
+    var strSemValor: String = ""
+    println(strSemValor)
+    val nmrDoCalcado: Byte = 41
+    println(nmrDoCalcado)
+    var pibBrasil: Long = 1869000000000
+    println(pibBrasil)
+    var qtdPupulacaoBrasil: Int = 211000000
+    println(qtdPupulacaoBrasil)
+}
