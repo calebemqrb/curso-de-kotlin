2382f3fc78055198572439a24b0c5e93053ff29a
Calebe-Marques <calebe.reboucas@sptech.school>
2024-01-20 23:28:47 -0300

    feat: validacao com when

---
+++ modulo-controle-de-fluxos/src/main/kotlin/Exercicios/When.kt
@@ -0,0 +1,22 @@
+package Exercicios
+
+fun bonusWhen(jobTitle: String): String{
+
+    return when(jobTitle.lowercase()){
+        "gerente" -> "R$2.000,00"
+        "coordenador" -> "R$1.500,00"
+        "engenheiro de software" -> "R$1.000,00"
+        "estagiário" -> "R$500,00"
+        else -> "R$0,00"
+    }
+}
+
+fun main(){
+    println(bonusWhen("Gerente"))
+
+    val bool = false
+    when(bool){
+        true -> println(bool)
+        false -> println(bool)
+    }
+}
