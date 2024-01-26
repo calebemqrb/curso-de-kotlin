6bfe747ac234560bc3121c05c03ffedb6df260ee
Calebe-Marques <calebe.reboucas@sptech.school>
2024-01-26 10:54:34 -0300

    feat: lacos de repeticao - while

---
+++ modulo-controle-de-fluxos/src/main/kotlin/While.kt
@@ -0,0 +1,15 @@
+fun main(){
+
+    val str = "String"
+    var i = 0
+    while (i < str.length){
+        println(str[i])
+        i++
+    }
+
+    var j = 10
+    while (j > 0){
+        println(j)
+        j--
+    }
+}
