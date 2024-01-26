852f2d818c562fa660f18b6ab4802aac8a59093a
Calebe-Marques <calebe.reboucas@sptech.school>
2024-01-26 10:47:24 -0300

    feat: lacos de repeticao - For

---
+++ modulo-controle-de-fluxos/src/main/kotlin/For.kt
@@ -0,0 +1,14 @@
+fun main(){
+    for(i in 0..20 step 2){
+        println(i)
+    }
+
+    val str = "Kotlin é show"
+    for(char in str){
+        println(char)
+    }
+
+    for(j in 20 downTo 0 step 3){
+        println(j)
+    }
+}
